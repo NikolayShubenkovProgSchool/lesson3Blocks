d2b4e71620827ffe4c54a2ecf405b03e44f9254c
NikolayShubenkovProgSchool <nshubenkov@NShubenkovs-MacBook-Pro.local>
2014-08-29 20:07:21 +0400

    Initial Commit

---
+++ PSRBlocks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PSRBlocks
+//
+//  Created by n.shubenkov on 29/08/14.
+//  Copyright (c) 2014 n.shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
